d65c7ea15f93228f575383f30d2a66525555b8fb
daniele <daniele.asciutti@mail.polimi.it>
2024-10-28 15:10:09 +0100

    added new docker-compose file for monitoring, added prometheus, cadvisor and node-exporter services. Adjusted grafana setup to handle another dashboard

--- WebPlatform/Frontend/vite-frontend/src/reports/constants.js
+++ WebPlatform/Frontend/vite-frontend/src/reports/constants.js
@@ -1,5 +1,10 @@
 // export const grafanaBaseUrl = 'http://localhost:3000/grafana/d-solo/bdm430davot1cb/devicetimeseries?orgId=1&var-learning_device_id=';
 
+// export const grafanaBaseUrl = 'http://localhost:3000/grafana/d-solo/bdm430davot1cb?orgId=1&var-learning_device_id=';
 
+// export const grafanaBaseUrl = 'http://localhost:3000/grafana/d-solo/bdm430davot1cb/devicetimeseries?orgId=1&var-learning_device_id=';
 
 export const grafanaBaseUrl = '/grafana/d-solo/bdm430davot1cb/devicetimeseries?orgId=1&var-learning_device_id=';
+
+// export const grafanaBaseUrl = '/grafana/d-solo/bdm430davot1cb/devicetimeseries?orgId=1&var-learning_device_id=';
+// export const grafanaBaseUrl = '/grafana/d/bdm430davot1cb/devicetimeseries?orgId=1&var-learning_device_id=';
